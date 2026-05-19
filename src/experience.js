2a9b9c789f64d454f80dabae6a371374d21bc9b2
Centiganda-851 <shanetendoj@gmail.com>
2026-05-19 12:42:57 -0400

    Updated to May26

--- src/experience.js
+++ src/experience.js
@@ -1,43 +1,43 @@
 const buttons = [
   {
-    selector: '.blocks.one',
+    selector: '.blocks.PhD',
+    text: 'Joined the Tucker Research Group at Clemson University to study quantum applications within industrial optimization, which are widely recognized as a very promising area for quantum research that would allow for faster iteration.',
+  },
+  {
+    selector: '.blocks.HM',
     text: 'During my internship at H+M, I designed electrical layouts for large distribution centers and industrial facilities, producing one-line diagrams, Revit models, and detailed lighting and receptacle plans.',
-    top: '50%',
-    left: '20%'
   },
   {
-    selector: '.blocks.two',
-    text: 'At iQuHack, my first hackathon, my team built a quantum algorithm for QuantumRings using Qbraid, aimed at efficiently factoring large semiprime numbers through hybrid quantum-classical methods. We ultimately managed to factor the number 143.',
-    top: '25%'
+    selector: '.blocks.IQ26',
+    text: 'Returning to iQuHack was a very different hacker. This time, the challenge was to invesitigate qubit entanglement with bell pairs competitively and I contributed circuit expertise. https://github.com/fwilhelmy/mit-iquhack-2026.git',
   },
   {
-    selector: '.blocks.three',
+    selector: '.blocks.IQ25',
+    text: 'At iQuHack, my first hackathon, my team built a quantum algorithm for QuantumRings using Qbraid, aimed at efficiently factoring large semiprime numbers through hybrid quantum-classical methods. We ultimately managed to factor the number 143. https://github.com/Centiganda851/2025-Quantum-Factorization-With-Quantum-Rings.git',
+  },
+  {
+    selector: '.blocks.Logos',
     text: 'Provided client support at Union University Library, assisting patrons in locating academic resources, answering research-related questions, and resolving technical or access issues promptly and professionally.',
-    top: '50%',
-    left: '80%'
   },
   {
-    selector: '.blocks.four',
+    selector: '.blocks.CAS',
+    text: 'Was a peer tutor, encouraging others studiying Physics and engineering and encouraging problem-solving approaches for people new to the fields.',
+  },
+  {
+    selector: '.blocks.ScP',
     text: 'Developed a dynamic PHP-based web application for Scorpius Pharmacy to monitor inventory, track employee activity, and manage sales data, improving efficiency and real-time business oversight.',
-    top: '50%',
-    left: '20%'
   },
   {
-    selector: '.blocks.five',
+    selector: '.blocks.QxQ',
     text: 'Participated in an IBM Quantum program led by Dr. Akshay Agarwal and Dr. Giselle Fontes Evilsizer, exploring quantum computing fundamentals, including qubits, quantum gates, and core computational algorithms such as Shor, Grover and Deutsch-Jozsa.',
-    top: '85%'
   },
   {
-    selector: '.blocks.six',
+    selector: '.blocks.AF',
     text: 'Completed a 2021 summer internship with the Aga Khan Development Network IT Department, gaining hands-on experience in campus network configuration, secure Wi-Fi implementation, and server maintenance operations.',
-    top: '50%',
-    left: '80%'
   },
   {
-    selector: '.blocks.seven',
-    text: 'Participated in a second quantum hackathon focused on leveraging quantum computing to train machine learning models capable of classifying tic-tac-toe outcomes—identifying wins, losses, and draws with 52% accuracy.',
-    top: '50%',
-    left: '80%'
+    selector: '.blocks.SC25',
+    text: 'Participated in a second quantum hackathon focused on leveraging quantum computing to train machine learning models capable of classifying tic-tac-toe outcomes—identifying wins, losses, and draws with 52% accuracy. https://github.com/Centiganda851/SC-Quant.git',
   }
 ];
 
@@ -59,20 +59,6 @@ function reset() {
   backgroundText.style.opacity = '0';
   borderText.style.opacity = '0';
   middleCam = false;
-  setTimeout(() => {
-    buttons[0].el.style.top = '35%';
-    buttons[0].el.style.left = '25%';
-    buttons[1].el.style.top = '35%';
-    buttons[1].el.style.left = '50%';
-    buttons[2].el.style.top = '35%';
-    buttons[2].el.style.left = '75%';
-    buttons[3].el.style.top = '75%';
-    buttons[3].el.style.left = '25%';
-    buttons[4].el.style.top = '75%';
-    buttons[4].el.style.left = '50%';
-    buttons[5].el.style.top = '75%';
-    buttons[5].el.style.left = '75%';
-  }, 500);
   setTimeout(() => {
     backgroundGrey.style.display = 'none';
     backgroundText.style.display = 'none';
